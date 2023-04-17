974a1dc9d619263e7782becaae6d311766d22903
赵兴 <mosaic.bilibili@gmail.com>
2023-04-17 18:11:29 +0800

    ✨ feat: 添加icon组件

--- play/app.vue
+++ play/app.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <h1>测试</h1>
-    <m-icon color="red" :size="30">你好</m-icon>
+    <m-icon color="red" :size="30" class="m-icon-search"></m-icon>
+    <h1>hello</h1>
   </div>
 </template>
 
--- play/main.ts
+++ play/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./app.vue";
 import { MIcon } from "@mosaic-plus/components/icon";
+import '@mosaic-plus/theme-chalk/src/index.scss';
 
 const app = createApp(App);
 app.use(MIcon)
