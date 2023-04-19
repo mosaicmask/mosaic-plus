6b76b6b1ce63eed9032e7ab563324017c8fd94d5
赵兴 <mosaic.bilibili@gmail.com>
2023-04-19 17:09:36 +0800

    📃 docs: 更新引用方式

--- play/app.vue
+++ play/app.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script setup lang="ts">
-
+import { MIcon } from "@mosaic-plus/components/icon";
 </script>
 
 <style lang="scss" scoped>
--- play/main.ts
+++ play/main.ts
@@ -1,8 +1,6 @@
 import { createApp } from "vue";
 import App from "./app.vue";
-import { MIcon } from "@mosaic-plus/components/icon";
 import '@mosaic-plus/theme-chalk/src/index.scss';
 
 const app = createApp(App);
-app.use(MIcon)
 app.mount("#app");
